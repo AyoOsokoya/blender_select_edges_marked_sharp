b39bfee9a4b7acbe01a060aafdbde2d2bdc864a8
Ayodeji Osokoya <inspireology@gmail.com>
2018-08-08 13:50:17 +0900

    Working implementation of plugin.

--- select_sharp_edges.py
+++ select_sharp_edges.py
@@ -1,10 +1,8 @@
 #https://blender.stackexchange.com/questions/41351/is-there-a-way-to-select-edges-marked-as-sharp-via-python/41353#41353
 #https://docs.blender.org/api/blender_python_api_2_65_5/info_tutorial_addon.html
 
-#TODO: Add to the Select Menu in edit mode
 #TODO: Add description to the addon
-#TODO: Break apart in to files
-#TODO: Experiment with Bpy Context
+#TODO: Break apart into files
 
 bl_info = {
     "name": "Select Edges Marked Sharp",
@@ -14,8 +12,8 @@ bl_info = {
     "location": "Edit Mode > Select Menu", #TODO: fix this
     "description": "Select all edges marked as sharp",
     "warning": "",
-    "wiki_url": "https://wiki.blender.org/index.php/Extensions:2.6/"
-                "Py/Scripts/Modeling/Extra_Tools",
+    "wiki_url": ""
+                "",
     "category": "Mesh"
 }
 
@@ -23,12 +21,12 @@ import bpy
 import bmesh
 
 class SelectSharpEdges(bpy.types.Operator):
-    """Select Edges Marked Sharp"""      # blender will use this as a tooltip for menu items and buttons.
-    bl_idname = "object.select_edges_marked_sharp"        # unique identifier for buttons and menu items to reference.
-    bl_label = "Select Edges Marked Sharp"         # display name in the interface.
-    bl_options = {'REGISTER', 'UNDO'}  # enable undo for the operator.
+    """Select Edges Marked Sharp""" #tooltip for menu items and buttons.
+    bl_idname = "object.select_edges_marked_sharp" # unique identifier for buttons and menu items to reference.
+    bl_label = "Select Edges Marked Sharp" # display name in the interface.
+    bl_options = {'REGISTER', 'UNDO'}
 
-    def execute(self, context):        # execute() is called by blender when running the operator.
+    def execute(self, context):
         obj = bpy.context.edit_object
         me = obj.data
 
@@ -39,7 +37,7 @@ class SelectSharpEdges(bpy.types.Operator):
 
         bmesh.update_edit_mesh(me, False)
 
-        return {'FINISHED'}            # this lets blender know the operator finished successfully.
+        return {'FINISHED'}
 
 
 
