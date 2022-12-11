a70c2957e831c12c45f44e5463b00073552e68b7
JeBobs <jebcraftserver@gmail.com>
2022-12-11 13:11:57 -0500

    (0.3) Support for selecting edges marked as a seam

--- select_sharp_edges.py
+++ select_sharp_edges.py
@@ -4,12 +4,12 @@
 #TODO: Add description to the addon
 
 bl_info = {
-    "name": "Select Edges Marked Sharp",
+    "name": "Select Edges Marked Sharp & Seam",
     "author": "Ayodeji Osokoya",
     "version": (0, 2),
     "blender": (2, 80, 0),
     "location": "Edit Mode > Select Menu", #TODO: fix this
-    "description": "Select all edges marked as sharp",
+    "description": "Select all edges marked as sharp or as seam",
     "warning": "",
     "wiki_url": "",
     "category": "Mesh"
@@ -26,27 +26,50 @@ class SelectSharpEdges(bpy.types.Operator):
 
     def execute(self, context):
         objects = bpy.context.selected_editable_objects
+        
         for obj in objects:
-            me = obj.data
-            bm = bmesh.from_edit_mesh(me)
+            bm = bmesh.from_edit_mesh(obj.data)
 
             for e in bm.edges:
                 if not e.smooth:
                     e.select = True
-            bmesh.update_edit_mesh(me)
+
+            bmesh.update_edit_mesh(obj.data)
+
         return {'FINISHED'}
 
+class SelectSeamEdges(bpy.types.Operator):
+    """Select Edges Marked Seam"""
+    bl_idname = "object.select_edges_marked_seam"
+    bl_label = "Select Edges Marked Seam"
+    bl_options = {'REGISTER', 'UNDO'}
 
+    def execute(self, context):
+        objects = bpy.context.selected_editable_objects
+
+        for obj in objects:
+            bm = bmesh.from_edit_mesh(obj.data)
+
+            for e in bm.edges:
+                if e.seam:
+                    e.select = True
+
+            bmesh.update_edit_mesh(obj.data)
+
+        return {'FINISHED'}
 
 def register():
     bpy.utils.register_class(SelectSharpEdges)
+    bpy.utils.register_class(SelectSeamEdges)
     bpy.types.VIEW3D_MT_select_edit_mesh.append(menu_func)
 
 def unregister():
     bpy.utils.unregister_class(SelectSharpEdges)
+    bpy.utils.unregister_class(SelectSeamEdges)
 
 def menu_func(self, context):
     self.layout.operator(SelectSharpEdges.bl_idname)
+    self.layout.operator(SelectSeamEdges.bl_idname)
 
 # This allows you to run the script directly from blenders text editor
 # to test the addon without having to install it.
